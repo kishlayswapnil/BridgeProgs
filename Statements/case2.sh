99f2712b0ed79d0a43b67859cee8e20b2b6fc1f6
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-07 09:55:50 +0530

    Added: Second problem on case statement has been added.

---
+++ Statements/case2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+echo "Enter a single digit from 1-7 only"
+read a
+case "$a" in
+"1") echo "Sunday"
+;;
+"2") echo "Monday"
+;;
+"3") echo "Tuesday"
+;;
+"4") echo "Wednesday"
+;;
+"5") echo "Thursday"
+;;
+"6") echo "Friday"
+;;
+"7") echo "Saturday"
+;;
+*) echo "Invalid Input"
+;;
+esac
